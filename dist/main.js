61313774613067fca1e141826f87fc8f7d9d220e
yasmeenattallah <yattallah7@gmail.com>
2022-12-13 23:33:23 +0200

    confirmpassword validation edit

--- dist/main.js
+++ dist/main.js
@@ -110,7 +110,7 @@ const checkConfirmPassword = (confirmErr) => {
         error.textContent = confirmErr?confirmErr:'The password does not match';
       }else {
         const error = document.getElementById('confirmError');
-        error.textContent = ' ';
+        error.textContent = confirmErr?confirmErr:' ';
         valid = true;
     }
 
@@ -149,12 +149,15 @@ registerForm.addEventListener('submit', (e)=>{
     }
      ).then((response)=>{
         const data = JSON.parse(response);
+        console.log(data.errors,777);
+
              localStorage.setItem("email", email);
              if (data.errors){
                 checkUsername( data.errors.username);
                  checkEmail(data.errors.email);
-                checkPassword(data.errors.password[1]);
-                 checkConfirmPassword(data.errors.password[0]);
+                checkPassword(data.errors.password[0]);
+                 checkConfirmPassword(data.errors.password[1]);
+                 console.log(data.errors.password,777);
             } else {
                  window.location.href="./login.html" ;
                
